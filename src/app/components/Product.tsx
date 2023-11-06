2cb86284705a9050005a6a76a52c4d9003b8f771
darlan-vieira <darlanvieira06@gmail.com>
2023-11-06 20:39:28 -0300

    Adicionando loading para pagina inicial

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,5 +1,6 @@
 import { ProductType } from "@/types/ProductType"
 import ProductImage from "./ProductImage"
+import { formatPrice } from "@/lib/utils"
 
 type ProductProps = {
   product: ProductType
@@ -11,7 +12,10 @@ export default function Product({ product}: ProductProps) {
       <div className="relative max-h-72 flex-1">
         <ProductImage product={product} fill />
       </div>
-      <div className="flex justify-between font-bold my-3">{product.title}</div>
+      <div className="flex justify-between font-bold my-3">
+        <p className="w-40 truncate">{product.name}</p>
+        <p className="text-md text-teal-300">{formatPrice(product.price)}</p>
+      </div>
       <button className="rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center">Adicionar ao Carrinho</button>
     </div>
   )
